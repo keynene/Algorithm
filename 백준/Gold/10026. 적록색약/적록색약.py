3e0cdefa16c2884ad9d587cfc8deeb1eacb61aa3
keynene <109068358+keynene@users.noreply.github.com>
2022-11-02 16:15:02 +0900

    [Gold V] Title: 적록색약, Time: 300 ms, Memory: 316800 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,54 +1,54 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(100000)
 
 n = int(input().rstrip())
-graph = [list(input().rstrip()) for _ in range(n)]
+rgb = [list(map(str, input().rstrip())) for _ in range(n)]
 amap = []
-rgmap = []
-acnt = 0
-rgcnt = 0
+visited = [[0 for _ in range(n)] for _ in range(n)]
+cnt = 0
+res = []
 
-def dfs(x,y,map,num):
+def dfs(x,y,num):
     if x<0 or y<0 or x>=n or y>=n:
         return
-    if map[y][x] == 3 and map[y][x] != num:
+    if visited[y][x] == 1 or amap[y][x] != num:
         return
-    elif map[y][x] == num:
-        map[y][x] = 3
-        dfs(x-1,y,map,num)
-        dfs(x,y-1,map,num)
-        dfs(x+1,y,map,num)
-        dfs(x,y+1,map,num)
+    elif amap[y][x] == num:
+        visited[y][x] = 1
+        if num == 2:
+            amap[y][x] = 0
+        dfs(x-1,y,num)
+        dfs(x,y-1,num)
+        dfs(x+1,y,num)
+        dfs(x,y+1,num)
         return
-    else:
-        return
+    
 
-
-for rgb in graph:
-    string = 0
+for string in rgb:
     num = ''
-    rgnum = ''
-    for string in rgb:
-        if string == 'R':
+    for al in string:
+        if al == 'R':
             num += str(0)+' '
-            rgnum += str(0)+' '
-        elif string == 'B':
+        elif al == 'B':
             num += str(1)+' '
-            rgnum += str(1)+' '
-        elif string == 'G':
+        elif al == 'G':
             num += str(2)+' '
-            rgnum += str(0)+' '
     amap.append(list(map(int, num.split())))
-    rgmap.append(list(map(int, rgnum.split())))
 
 for y in range(n):
     for x in range(n):
-        if amap[y][x] != 3:
-            dfs(x,y,amap,amap[y][x])
-            acnt += 1
-        if rgmap[y][x] != 3:
-            dfs(x,y,rgmap,rgmap[y][x])
-            rgcnt += 1
+        if not visited[y][x]:
+            dfs(x,y,amap[y][x])
+            cnt += 1
+res.append(cnt)
 
-print(acnt, rgcnt,sep=' ')
+cnt = 0
+visited = [[0 for _ in range(n)] for _ in range(n)]
+for y in range(n):
+    for x in range(n):
+        if not visited[y][x]:
+            dfs(x,y,amap[y][x])
+            cnt += 1
+res.append(cnt)
+print(*res)
