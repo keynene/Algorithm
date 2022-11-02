5dd750cf5b7fe2466af42c5227b7d9d0b6b8f9b1
keynene <109068358+keynene@users.noreply.github.com>
2022-11-02 16:57:03 +0900

    [Gold V] Title: 적록색약, Time: 256 ms, Memory: 267652 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -2,53 +2,38 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(100000)
 
+def dfs(x,y):
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    visited[y][x] = True
+    for i in range(4):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        if 0<=nx<n and 0<=ny<n and amap[y][x] == amap[ny][nx] and not visited[ny][nx]:
+            dfs(nx,ny)
+
 n = int(input().rstrip())
-rgb = [list(map(str, input().rstrip())) for _ in range(n)]
-amap = []
-visited = [[0 for _ in range(n)] for _ in range(n)]
-cnt = 0
-res = []
-
-def dfs(x,y,num):
-    if x<0 or y<0 or x>=n or y>=n:
-        return
-    if visited[y][x] == 1 or amap[y][x] != num:
-        return
-    elif amap[y][x] == num:
-        visited[y][x] = 1
-        if num == 2:
-            amap[y][x] = 0
-        dfs(x-1,y,num)
-        dfs(x,y-1,num)
-        dfs(x+1,y,num)
-        dfs(x,y+1,num)
-        return
-    
-
-for string in rgb:
-    num = ''
-    for al in string:
-        if al == 'R':
-            num += str(0)+' '
-        elif al == 'B':
-            num += str(1)+' '
-        elif al == 'G':
-            num += str(2)+' '
-    amap.append(list(map(int, num.split())))
+amap = [list(map(str, input().rstrip())) for _ in range(n)]
+visited = [[False]*n for _ in range(n)]
+acnt = 0
 
 for y in range(n):
     for x in range(n):
         if not visited[y][x]:
-            dfs(x,y,amap[y][x])
-            cnt += 1
-res.append(cnt)
+            dfs(x,y)
+            acnt += 1
 
-cnt = 0
-visited = [[0 for _ in range(n)] for _ in range(n)]
+for y in range(n):
+    for x in range(n):
+        if amap[y][x] == 'R':
+            amap[y][x] = 'G'
+
+rgcnt = 0
+visited = [[False]*n for _ in range(n)]
 for y in range(n):
     for x in range(n):
         if not visited[y][x]:
-            dfs(x,y,amap[y][x])
-            cnt += 1
-res.append(cnt)
-print(*res)
+            dfs(x,y)
+            rgcnt += 1
+
+print(acnt, rgcnt)
