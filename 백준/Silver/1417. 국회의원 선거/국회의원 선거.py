0da0374b6f219896f8fc4146d68fc7ec710212ee
keynene <109068358+keynene@users.noreply.github.com>
2022-11-12 23:15:57 +0900

    [Silver V] Title: 국회의원 선거, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,22 +1,21 @@
-
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 vote = [0]+[int(input().rstrip()) for _ in range(N)]
 buy = 0
-idx = 0
 
 while True:
-    maxV = max(vote)
-    if vote.count(maxV) == 1 and vote.index(maxV) == 1:
+    if vote.index(max(vote)) == 1 and vote.count(max(vote)) == 1:
         print(buy)
         break
     else:
-        for i in range(2,len(vote)):
-            if vote[i] == maxV:
-                idx = i
-                break
-        vote[idx] -= 1
+        maxV = 0
+        maxidx = 0
+        for i in range(2, N+1):
+            if vote[i] > maxV:
+                maxV = vote[i]
+                maxidx = i
+        vote[maxidx] -= 1
         vote[1] += 1
         buy += 1
