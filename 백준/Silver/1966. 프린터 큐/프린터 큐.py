8a565590581d7d5e94c0e91e8848137cc5987748
keynene <109068358+keynene@users.noreply.github.com>
2022-11-12 22:29:35 +0900

    [Silver III] Title: 프린터 큐, Time: 160 ms, Memory: 115636 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -1,29 +1,26 @@
 import sys
 from collections import deque
-from copy import deepcopy
 input = sys.stdin.readline
 
-def order(m,doc):
-    queue = deque()
-    for i in range(len(doc)):
-        queue.append((i, doc[i]))
-    comp = deepcopy(doc)
-    comp_doc = comp.sort(key=lambda x:-x)
-    cnt = 1
-    
-    while queue:
-        idx,docu = queue.popleft()
-        if comp[0] == docu:
-            if idx == m:
-                print(cnt)
-                return
-            else:
-                del comp[0]
-                cnt += 1
-        else:
-            queue.append((idx,docu))
-
 for _ in range(int(input())):
-    N,M = map(int,input().split())
-    document = list(map(int, input().split()))
-    order(M,document)
+    N, M = map(int,input().split())
+    imfort = list(map(int, input().split()))
+    cnt = 0
+
+    queue = deque()
+
+    for i in range(N):
+        queue.append((i, imfort[i]))
+
+    while queue:
+        idx, imfo = queue.popleft()
+
+        if imfo == max(imfort):
+            cnt += 1
+            if idx == M:
+                print(cnt)
+                break
+            else:
+                imfort[idx] = 0
+        else:
+            queue.append((idx, imfo))
