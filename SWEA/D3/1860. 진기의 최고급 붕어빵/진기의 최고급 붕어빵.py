34c16495324972973aea43dcca7b5561b0c97d50
keynene <109068358+keynene@users.noreply.github.com>
2022-11-16 02:20:28 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 341 ms, Memory: 62,084 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.py
@@ -1,24 +1,13 @@
-for t in range(1,int(input())+1):
+for tc in range(1,int(input())+1):
     N,M,K = map(int,input().split())
-    c = sorted(list(map(int,input().split())))
+    lst = sorted(list(map(int,input().split())))
+
+    ans = 'Possible'
     cnt = 0
-    make = [0]*(c[-1]+1)
-
-    for i in range(1,len(make)):
-        if i%M == 0:
-            cnt += 1
-            make[i] = K*cnt
-        else: make[i] = make[i-1]
-    
-    suc = 0
-
-    for arrive in c:
-        if make[arrive] > 0:
-            for i in range(arrive,len(make)):
-                make[i] -= 1
-            suc += 1
-        else:
+    for t in lst:
+        cnt += 1
+        if (t//M)*K < cnt:
+            ans = 'Impossible'
             break
-    
-    if suc == N: print(f'#{t} Possible')
-    else: print(f'#{t} Impossible')
+
+    print(f'#{tc} {ans}')
