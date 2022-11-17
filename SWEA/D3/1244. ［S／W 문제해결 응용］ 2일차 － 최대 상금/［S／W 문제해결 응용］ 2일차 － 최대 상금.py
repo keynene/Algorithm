1e671bcdab7ff1580a7d94bf3249b4824a353f27
keynene <109068358+keynene@users.noreply.github.com>
2022-11-17 16:35:45 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 467 ms, Memory: 60,916 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,28 +1,30 @@
 def dfs(n):
     global ans
     if n == N:
-        ans = max(ans, int("".join(map(str,lst))))
+        ans = max(ans, int("".join(map(str,arr))))
         return
     
     for i in range(L-1):
-        for j in range(i+1, L):
-            lst[i], lst[j] = lst[j], lst[i]
+        for j in range(i+1,L):
+            arr[i], arr[j] = arr[j], arr[i]
 
-            chk = int("".join(map(str,lst)))
+            chk = int("".join(map(str,arr)))
             if (n, chk) not in V:
                 dfs(n+1)
                 V.append((n,chk))
 
-            lst[i], lst[j] = lst[j], lst[i]
+            arr[i], arr[j] = arr[j], arr[i]
 
-for tc in range(int(input())):
-    st, t = input().split()
-    N = int(t)
-    lst = []
-    for ch in st:
-        lst.append(int(ch))
-    L = len(lst)
-    ans = 0
+for t in range(int(input())):
+    Num, N = map(str,input().split())
+    N = int(N)
+    arr = []
     V = []
+    for i in Num:
+        arr.append(int(i))
+    L = len(arr)
+    ans = 0
+
     dfs(0)
-    print(f'#{tc+1} {ans}')
+
+    print(f'#{t+1} {ans}')
