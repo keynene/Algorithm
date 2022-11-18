47173aef169945f25aa6ac0652619f916235216f
keynene <109068358+keynene@users.noreply.github.com>
2022-11-18 23:01:26 +0900

    [D3] Title: 오목 판정, Time: 190 ms, Memory: 60,684 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,28 +1,28 @@
-def move(arr):
+dx = [-1,0,1,1]
+dy = [-1,-1,-1,0]
+
+def check(x,y):
     global ans
-    dr = [0,1,1,1]
-    dc = [1,0,1,-1]
-    for i in range(N):
-        for j in range(N):
-            if arr[i][j] == 'o':
-                for d in range(4):
-                    r = i
-                    c = j
-                    cnt = 0
-                    while 0<=r<N and 0<=c<N and arr[r][c]=='o':
-                        cnt += 1
-                        r += dr[d]
-                        c += dc[d]
-                    if cnt >=5:
-                        ans = 'YES'
-                        return 
-    return 
+    for i in range(4):
+        nx = x
+        ny = y
+        cnt = 0
+        
+        while 0<=nx<N and 0<=ny<N and arr[ny][nx] == 'o':
+            cnt += 1
+            nx += dx[i]
+            ny += dy[i]
+        if cnt >= 5:
+            ans = 'YES'
+            return
 
 for t in range(int(input())):
     N = int(input())
-    arr = [input() for _ in range(N)]
+    arr = [list(map(str, input())) for _ in range(N)]
     ans = 'NO'
 
-    move(arr)
+    for y in range(N):
+        for x in range(N):
+            check(x,y)
 
     print(f'#{t+1} {ans}')
