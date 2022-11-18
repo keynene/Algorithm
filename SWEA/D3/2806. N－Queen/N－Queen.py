3b8ca55eac92600eb1c2bd8c18fe6f95dcb51f04
keynene <109068358+keynene@users.noreply.github.com>
2022-11-19 01:41:18 +0900

    [D3] Title: N-Queen, Time: 167 ms, Memory: 61,284 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -0,0 +1,19 @@
+def dfs(n):
+    global ans
+    if n == N:
+        ans += 1
+        return
+    
+    for j in range(N):
+        if v1[j] == v2[n+j] == v3[n-j] == 0:
+            v1[j] = v2[n+j] = v3[n-j] = 1
+            dfs(n+1)
+            v1[j] = v2[n+j] = v3[n-j] = 0
+
+for t in range(int(input())):
+    N = int(input())
+    ans = 0
+
+    v1,v2,v3 = [[0]*(2*N) for _ in range(3)]
+    dfs(0)
+    print(f'#{t+1} {ans}')
