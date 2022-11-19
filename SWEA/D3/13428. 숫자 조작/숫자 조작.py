d6f6e36bbbef96c691739a5394eef3f16bba17bb
keynene <109068358+keynene@users.noreply.github.com>
2022-11-19 16:15:10 +0900

    [D3] Title: 숫자 조작, Time: 215 ms, Memory: 63,452 KB -BaekjoonHub

--- SWEA/D3/13428. 숫자 조작/숫자 조작.py
+++ SWEA/D3/13428. 숫자 조작/숫자 조작.py
@@ -1,30 +1,31 @@
+from copy import deepcopy
+
 def dfs(n):
     global maxR, minR
-    if n == 1:
+    if n > 0:
         maxR = max(maxR, int("".join(map(str,arr))))
         minR = min(minR, int("".join(map(str,arr))))
         return
-
-    for i in range(L-1):
+    
+    for i in range(L):
         for j in range(i+1,L):
             arr[i], arr[j] = arr[j], arr[i]
-            if arr[0] == 0:
-                arr[i], arr[j] = arr[j], arr[i]
-                continue
-
-            chk = int("".join(map(str,arr)))
-            if (n, chk) not in V:
-                dfs(n+1)
-                V.append((n,chk))
-
+            if arr[0] != 0:
+                chk = int("".join(map(str,arr)))
+                if (n,chk) not in v:
+                    v.append((n,chk))
+                    dfs(n+1)
             arr[i], arr[j] = arr[j], arr[i]
 
+
 for t in range(int(input())):
-    maxR = int(input())
-    minR = maxR
-    arr = list(map(int, str(maxR)))
-    L = len(arr)
-    V = []
+
+    Num = input()
+    N = int(Num)  #숫자 12345
+    minR = maxR =int(Num)
+    L = len(Num)  #종료길이
+    arr = list(map(int, str(Num)))
+    v = []
 
     dfs(0)
 
