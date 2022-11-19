535b343db74360e3eefb8a64cfee5b54981d3a02
keynene <109068358+keynene@users.noreply.github.com>
2022-11-19 20:15:49 +0900

    [D3] Title: N-Queen, Time: 818 ms, Memory: 74,932 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.py
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -1,19 +1,42 @@
-def dfs(n):
-    global ans
+from copy import deepcopy
+
+dx = [1,1,0,-1,-1,-1,0,1]
+dy = [0,-1,-1,-1,0,1,1,1]
+
+def visit(x,y,arr):
+    varr = deepcopy(arr)
+    for i in range(8):
+        nx = x
+        ny = y
+        while 0<=nx<N and 0<=ny<N:
+            varr[ny][nx] = True
+            nx += dx[i]
+            ny += dy[i]
+    return varr
+
+def dfs(n,chess):
+    global ans,cnt
+
     if n == N:
         ans += 1
         return
-    
-    for j in range(N):
-        if v1[j] == v2[n+j] == v3[n-j] == 0:
-            v1[j] = v2[n+j] = v3[n-j] = 1
-            dfs(n+1)
-            v1[j] = v2[n+j] = v3[n-j] = 0
+
+    for y in range(n,N):
+        cnt = 0
+        for x in range(N):
+            if not chess[y][x]:
+                cnt += 1
+                temp = visit(x,y,chess)
+                dfs(n+1,temp)
+        if cnt < 1:
+            break
 
 for t in range(int(input())):
     N = int(input())
+    chess = [[False]*N for _ in range(N)]
     ans = 0
+    cnt = 0
 
-    v1,v2,v3 = [[0]*(2*N) for _ in range(3)]
-    dfs(0)
+    dfs(0,chess)
+
     print(f'#{t+1} {ans}')
