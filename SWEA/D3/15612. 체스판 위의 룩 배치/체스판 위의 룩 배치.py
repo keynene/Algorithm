729f25ed28ee647b234e17479064e7cbc81aad4b
keynene <109068358+keynene@users.noreply.github.com>
2022-11-20 00:51:21 +0900

    [D3] Title: 체스판 위의 룩 배치, Time: 136 ms, Memory: 57,904 KB -BaekjoonHub

--- SWEA/D3/15612. 체스판 위의 룩 배치/체스판 위의 룩 배치.py
+++ SWEA/D3/15612. 체스판 위의 룩 배치/체스판 위의 룩 배치.py
@@ -1,18 +1,12 @@
-dx = [1,0]
-dy = [0,1]
-
 def check(x,y):
     global ans
-    for i in range(2):
-        nx = x
-        ny = y
-        cnt = 0
-        while 0<=nx<N and 0<=ny<N:
-            if chess[ny][nx] == 'O':
-                cnt += 1
-            nx += dx[i]
-            ny += dy[i]
-        if cnt >= 2:
+    for i in range(x+1,N):
+        if chess[y][i] == 'O':
+            ans = 'no'
+            return
+    
+    for j in range(y+1,N):
+        if chess[j][x] == 'O':
             ans = 'no'
             return
 
@@ -27,8 +21,8 @@ for t in range(int(input())):
             if chess[y][x] == 'O':
                 check(x,y)
                 lcnt += 1
-    
+
     if lcnt != 8:
         ans = 'no'
-
+    
     print(f'#{t+1} {ans}')
