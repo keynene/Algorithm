776b8890b0f4d52378ce52fa7bc6d88ad8d3206a
keynene <109068358+keynene@users.noreply.github.com>
2023-05-16 16:45:13 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 160 ms, Memory: 60,088 KB -BaekjoonHub

---
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
@@ -0,0 +1,13 @@
+for T in range(1,int(input())+1):
+  N = int(input())
+  arr = [0] + list(map(int, input().split()))
+  dp = [0]*(N+1)
+
+  for i in range(1,N+1):
+    mx = 0
+    for j in range(0,i):
+      if arr[i] > arr[j]:
+        mx = max(mx, dp[j])
+    dp[i] = mx+1
+
+  print(f'#{T} {max(dp)}')
