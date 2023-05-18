2a1021e04796a47a428e19d5f84da1e9a4b18508
keynene <109068358+keynene@users.noreply.github.com>
2023-05-18 17:05:06 +0900

    [D3] Title: 농작물 수확하기, Time: 194 ms, Memory: 61,680 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,15 +1,14 @@
-for t in range(int(input())):
-    N = int(input())
-    M = N//2
-    arr = [list(map(int, str(input()))) for _ in range(N)]
-    ans = 0
+for T in range(int(input())):
+  N = int(input())
+  m = N//2
+  arr = [list(map(int, input())) for _ in range(N)]
+  ans = 0
 
-    for i in range(N):
-        if i <= M:
-            for j in range(M-i,M+i+1):
-                ans += arr[i][j]
-        else:
-            for j in range(i-M, N-(i-M)):
-                ans += arr[i][j]
-
-    print(f'#{t+1} {ans}')
+  for i in range(N):
+    if i <= m:
+      for j in range(m-i, m+i+1):
+        ans += arr[i][j]
+    else:
+      for j in range(i-m,N-(i-m)):
+        ans += arr[i][j]
+  print(f'#{T+1} {ans}')
