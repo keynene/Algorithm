b90ba0bfa46069f23965ee873187840a9d12333b
keynene <109068358+keynene@users.noreply.github.com>
2023-05-20 01:18:16 +0900

    [D3] Title: 최장 경로, Time: 153 ms, Memory: 60,736 KB -BaekjoonHub

--- SWEA/D3/2814. 최장 경로/최장 경로.py
+++ SWEA/D3/2814. 최장 경로/최장 경로.py
@@ -1,26 +1,23 @@
-from collections import deque
-
-def dfs(i,v):
+def dfs(s,v):
   global ans
-  ans = max(ans, len(v))
+  ans = max(ans,len(v))
 
-  for s, e in edge:
-    if i == s and e not in v:
+  for e in graph[s]:
+    if e not in v:
       dfs(e,v+[e])
-  
+
 
 for T in range(int(input())):
   N,M = map(int, input().split())
-  edge = [list(map(int, input().split())) for _ in range(M)]
-  v = []
+  graph = [[] for _ in range(N+1)]
   ans = 0
 
-  for x,y in edge:
-    if [y,x] not in edge:
-      edge.append([y,x])
-  edge.sort()
+  for _ in range(M):
+    u,v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-  for i in range(1,N+1):
-    dfs(i,v+[i])
+  for s in range(1,N+1):
+    dfs(s,[s])
 
   print(f'#{T+1} {ans}')
