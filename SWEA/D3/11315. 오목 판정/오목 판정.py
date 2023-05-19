b443b5e8c4ecdf9324350b82a30e4a87ea8fcb60
keynene <109068358+keynene@users.noreply.github.com>
2023-05-19 15:57:25 +0900

    [D3] Title: 오목 판정, Time: 211 ms, Memory: 60,888 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,18 +1,21 @@
 dx = [1,1,0,-1]
 dy = [0,-1,-1,-1]
 
-def dfs(x,y,dx,dy,d):
+def dfs(x,y):
   global ans
-  if d >= 5:
-    ans = 'YES'
-    return
-  
-  else:
-    nx = x+dx
-    ny = y+dy
-    if 0<=nx<N and 0<=ny<N and arr[ny][nx] == 'o':
-      dfs(nx,ny,dx,dy,d+1)
-  
+  for i in range(4):
+    nx = x+dx[i]
+    ny = y+dy[i]
+    cnt = 1
+
+    while 0<=nx<N and 0<=ny<N and arr[ny][nx] == 'o':
+      cnt += 1
+      nx += dx[i]
+      ny += dy[i]
+    
+    if cnt >= 5:
+      ans = 'YES'
+      return
 
 
 for T in range(int(input())):
@@ -23,13 +26,6 @@ for T in range(int(input())):
   for y in range(N):
     for x in range(N):
       if arr[y][x] == 'o':
-        for i in range(4):
-          nx = x+dx[i]
-          ny = y+dy[i]
-          if 0<=nx<N and 0<=ny<N and arr[ny][nx] == 'o':
-            dfs(nx,ny,dx[i],dy[i],2)
-
-    if ans == 'YES':
-      break
+        dfs(x,y)
 
   print(f'#{T+1} {ans}')
