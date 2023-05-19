9e51c4733b144c7c9469cd9c5445dc5c2d296cac
keynene <109068358+keynene@users.noreply.github.com>
2023-05-19 19:19:20 +0900

    [D3] Title: 부분 수열의 합, Time: 1,005 ms, Memory: 64,800 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,19 +1,18 @@
-def dfs(n, sm):
-    global ans
+def dfs(n,sm):
+  global cnt
+  if n == N:
+    if sm == K:
+      cnt += 1
+    return
+  
+  dfs(n+1,sm+num[n])
+  dfs(n+1,sm)
 
-    if n == N:
-        if sm == K:
-            ans += 1
-        return
-    
-    dfs(n+1, sm+A[n])
-    dfs(n+1, sm)
+for T in range(int(input())):
+  N, K = map(int, input().split())
+  num = list(map(int,input().split()))
+  cnt = 0
 
-for t in range(1,int(input())+1):
-    N, K = map(int, input().split())
-    A = list(map(int, input().split()))
-    ans = 0
+  dfs(0,0)
 
-    dfs(0,0)
-
-    print(f'#{t} {ans}')
+  print(f'#{T+1} {cnt}')
