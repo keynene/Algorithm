02937160ec1ecf4f876700758fdbcb98253e1eda
keynene <109068358+keynene@users.noreply.github.com>
2023-05-19 15:48:05 +0900

    [D3] Title: 오목 판정, Time: 209 ms, Memory: 60,388 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,28 +1,35 @@
-dx = [-1,0,1,1]
-dy = [1,1,1,0]
+dx = [1,1,0,-1]
+dy = [0,-1,-1,-1]
 
-def dfs(x,y):
-    global ans
-    for i in range(4):
-        nx = x
-        ny = y
-        cnt = 0
-        while 0<=nx<N and 0<=ny<N and arr[ny][nx]=='o':
-            cnt += 1
-            nx += dx[i]
-            ny += dy[i]
-        if cnt >= 5:
-            ans = 'YES'
-            return
+def dfs(x,y,dx,dy,d):
+  global ans
+  if d >= 5:
+    ans = 'YES'
+    return
+  
+  else:
+    nx = x+dx
+    ny = y+dy
+    if 0<=nx<N and 0<=ny<N and arr[ny][nx] == 'o':
+      dfs(nx,ny,dx,dy,d+1)
+  
 
-for t in range(int(input())):
-    N = int(input())
-    arr = [list(map(str, input())) for _ in range(N)]
-    ans = 'NO'
 
-    for y in range(N):
-        for x in range(N):
-            if arr[y][x] == 'o':
-                dfs(x,y)
+for T in range(int(input())):
+  N = int(input())
+  arr = [list(map(str,input())) for _ in range(N)]
+  ans = 'NO'
 
-    print(f'#{t+1} {ans}')
+  for y in range(N):
+    for x in range(N):
+      if arr[y][x] == 'o':
+        for i in range(4):
+          nx = x+dx[i]
+          ny = y+dy[i]
+          if 0<=nx<N and 0<=ny<N and arr[ny][nx] == 'o':
+            dfs(nx,ny,dx[i],dy[i],2)
+
+    if ans == 'YES':
+      break
+
+  print(f'#{T+1} {ans}')
