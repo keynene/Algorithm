817753cd77aae1c17c61311523c55657ffd71753
keynene <109068358+keynene@users.noreply.github.com>
2023-05-21 01:40:18 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 516 ms, Memory: 61,720 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,25 +1,24 @@
 def dfs(n,v):
   global ans
   if n == cnt:
-    ans = max(ans,int("".join(map(str,arr))))
+    ans = max(ans, int("".join(map(str, arr))))
     return
   
   for i in range(L-1):
     for j in range(i+1,L):
       arr[i], arr[j] = arr[j], arr[i]
-      chk = int("".join(map(str,arr)))
+      chk = int("".join(map(str, arr)))
       if (n,chk) not in v and arr[0] != 0:
         v.append((n,chk))
         dfs(n+1,v)
       arr[i], arr[j] = arr[j], arr[i]
-      
 
 for T in range(int(input())):
   num, cnt = map(int, input().split())
   arr = list(map(int, str(num)))
   L = len(arr)
   ans = 0
-
+  
   dfs(0,[])
 
   print(f'#{T+1} {ans}')
