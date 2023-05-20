b048b35c9400475cb407af389dddc2dc3aa78bae
keynene <109068358+keynene@users.noreply.github.com>
2023-05-21 02:26:36 +0900

    [Silver I] Title: 나이트의 이동, Time: 304 ms, Memory: 128072 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -2,34 +2,37 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-dx = [-2, -1, 1, 2, 2, 1, -1, -2]
-dy = [1, 2, 2, 1, -1, -2, -2, -1]
+dx = [2,1,-1,-2,-2,-1,1,2]
+dy = [1,2,2,1,-1,-2,-2,-1]
 
-def bfs():
-    global cnt
-    queue = deque()
-    queue.append((sx,sy))
-    
-    while queue:
-        x,y = queue.popleft()
+def bfs(sx,sy):
+  global ans
+  if sx==ex and sy==ey:
+    return
+  queue = deque()
+  queue.append((sx,sy))
 
-        if x==ex and y==ey:
-            return chess[y][x]
+  while queue:
+    x,y = queue.popleft()
+    if x==ex and y==ey:
+      ans = chess[y][x]
+      return
 
-        for i in range(8):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            
-            if 0<=nx<n and 0<=ny<n:
-                if chess[ny][nx] == 0:
-                    chess[ny][nx] = chess[y][x]+1
-                    queue.append((nx,ny))
+    for i in range(8):
+      nx = x+dx[i]
+      ny = y+dy[i]
 
-for i in range(int(input())):
-    n = int(input())
-    chess = [[0 for _ in range(n)] for _ in range(n)]
-    sx, sy = map(int,input().split())
-    ex, ey = map(int,input().split())
+      if 0<=nx<N and 0<=ny<N and chess[ny][nx] == 0:
+        chess[ny][nx] = chess[y][x]+1
+        queue.append((nx,ny))
 
-    if sx == ex and sy == ey : print(0)
-    else: print(bfs())
+for T in range(int(input())):
+  N = int(input())
+  chess = [[0]*N for _ in range(N)]
+  sx,sy = map(int, input().split())
+  ex,ey = map(int, input().split())
+  ans = 0
+
+  bfs(sx,sy)
+
+  print(ans)
