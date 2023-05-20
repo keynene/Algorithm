16070a62f8268c0b88e1d87fdb4403ab802a9782
keynene <109068358+keynene@users.noreply.github.com>
2023-05-21 02:07:15 +0900

    [D3] Title: 햄버거 다이어트, Time: 201 ms, Memory: 63,388 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,12 +1,13 @@
-for T in range(1,int(input())+1):
-  N, L = map(int, input().split())
-  point = [list(map(int, input().split())) for _ in range(N)]
+for T in range(int(input())):
+  N,L = map(int, input().split())
+  ham = [list(map(int,input().split())) for _ in range(N)]
   dp = [[0]*(L+1) for _ in range(N+1)]
 
   for i in range(1,N+1):
     for j in range(1,L+1):
-      if j-point[i-1][1] >= 0:
-        dp[i][j] = max(point[i-1][0]+dp[i-1][j-point[i-1][1]],dp[i-1][j])
-      else: dp[i][j] = dp[i-1][j]
+      if j-ham[i-1][1] >= 0:
+        dp[i][j] = max(dp[i-1][j], dp[i-1][j-ham[i-1][1]]+ham[i-1][0])
+      else:
+        dp[i][j] = dp[i-1][j]
 
-  print('#{} {}'.format(T,dp[N][L]))
+  print(f'#{T+1} {dp[-1][-1]}')
