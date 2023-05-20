481e39458c1e2c40b2bbd03ec9bf35596ca4127d
keynene <109068358+keynene@users.noreply.github.com>
2023-05-21 00:31:51 +0900

    [D3] Title: 구구단 걷기, Time: 500 ms, Memory: 51,196 KB -BaekjoonHub

--- SWEA/D3/16800. 구구단 걷기/구구단 걷기.py
+++ SWEA/D3/16800. 구구단 걷기/구구단 걷기.py
@@ -1,12 +1,14 @@
 for T in range(int(input())):
   N = int(input())
   ans = 0
-  for a in range(1,int(N**(1/2))+1):
-    if N%a == 0:
-      b = N//a
-      if ans != 0:
-        ans = min(ans,a-1+b-1)
+
+  for i in range(1,int(N**(1/2))+1):
+    if N%i == 0:
+      j = N//i
+
+      if ans == 0:
+        ans = i-1+j-1
       else:
-        ans = a-1+b-1
+        ans = min(ans,i-1+j-1)
 
   print(f'#{T+1} {ans}')
